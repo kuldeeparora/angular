6c0b8d631afb52a86d7b6aeeefec81ba66012d36
Kuldeep <kuldeep.arora@gmail.com>
2014-06-16 00:54:15 +0100

    added more functionality on task

--- Javascript-Assignments/js/application/task.js
+++ Javascript-Assignments/js/application/task.js
@@ -1,51 +1,57 @@
 /* Task Management */
 
-var number = 0;
-
-function task(title, id, meeting, date) {
+function task(title, date, id) {
     this.title = title;
-    this.id = id;
-    this.meeting = meeting;
     this.date = date;
+    this.id = id;
 }
 
 var tasks = [];
+var taskTextbox = $('.task-textbox');
+var taskShow = $('.task-show');
+var meetingCheckbox = $('.meeting-checkbox');
+var dateBox = $('#date-box');
 
 var addTask = function () {
-    var taskTextbox = $('.task-textbox');
+    if(taskTextbox.val() == ""){
+        console.log('New Task Title should not be empty');
+        return;
+    };
+    if (meetingCheckbox.is(':checked')){
+        if(dateBox.val() == ""){
+            console.log('Date Box should not be empty');
+            return;
+        };
+    }
     var taskId = "task" + (tasks.length + 1);
-    var meetingCheckbox = $('.meeting-checkbox');
-    var dateBox = $('#date-box');
-    var newTask = new task(taskTextbox.val(), taskId, meetingCheckbox.val(), dateBox.val());
+    var newTask = new task(taskTextbox.val(), dateBox.val(), taskId);
     tasks.push(newTask);
     updateUI();
-};
+}
 
-var updateUI = function () {
-    $('.task-show').empty();
-    var tasksLength = tasks.length;
-    var removeBtn = "<a href='#' class='remove-btn' onclick='removeTask(event)'>Remove Task</a>";
-    for (var i = 0; i < tasksLength; i++) {
-        var appendTasks = "<div id='" + tasks[i].id + "'>" + tasks[i].title + " ------- " + tasks[i].date + "------------" + removeBtn + "</div>";
-        $('.task-show').append(appendTasks);
-    }
-};
-
-
-$('.meeting-checkbox').click(function () {
-    $('#date-box').toggle();
-});
-
-function removeTask(event) {
+var removeTask = function (event) {
     var rowId = $(event.target).parent().attr("id");
     var tasksLength = tasks.length;
-    for (var i = 0; i < tasksLength; i++) {
-        if (this.tasks[i].id == rowId) {
+    for (var i=0; i < tasksLength; i++ ){
+        if(this.tasks[i].id == rowId){
             var index = tasks.indexOf(tasks[i].id);
             tasks.splice(i, 1);
             updateUI();
             break;
         }
+    };
+}
 
+var updateUI = function () {
+    taskShow.empty();
+    var tasksLength = tasks.length;
+    var removeBtn = "<a href='#' class='remove-btn' onclick='removeTask(event)'>Remove Task</a>";
+    for (var i = 0; i < tasksLength; i++) {
+        var appendTasks = "<div id='" + tasks[i].id + "'>" + tasks[i].title + " ------- " + tasks[i].date + "------------" + removeBtn + "</div>";
+        taskShow.append(appendTasks);
     }
 }
+
+meetingCheckbox.click(function () {
+    dateBox.toggle();
+});

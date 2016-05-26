87fe2d2b29ba908c6d3305cba6bcde7c74301e56
Kuldeep Arora <kuldeep.arora@gmail.com>
2016-05-26 21:39:38 +0100

    React basic

---
+++ ReactJS/pluralsight/script/app.js
@@ -0,0 +1,20 @@
+var Button = React.createClass({
+    getInitialState: function () {
+      return {
+          counter : 0
+      }
+    },
+    handleClick: function () {
+      return (
+          this.setState({counter: this.state.counter+1})
+      )
+    },
+   render: function () {
+       return(
+           <button onClick={this.handleClick}>{ this.state.counter }</button>
+       )
+   }
+});
+
+ReactDOM.render(<Button />, document.getElementById("root"));
+

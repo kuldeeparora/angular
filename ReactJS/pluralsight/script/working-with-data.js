d3296eece639478e64dbec40220f2be7fdf62650
Kuldeep Arora <kuldeep.arora@gmail.com>
2016-05-29 21:26:32 +0100

    working with data complete

--- ReactJS/pluralsight/script/working-with-data.js
+++ ReactJS/pluralsight/script/working-with-data.js
@@ -19,12 +19,41 @@ var Card = React.createClass({
    } 
 });
 
+var Form = React.createClass({
+    handleSubmit : function(e){
+        e.preventDefault();
+        var loginInput = ReactDOM.findDOMNode(this.refs.login);
+        this.props.addCard(loginInput.value);
+        loginInput.value = '';
+
+    },
+    render :function () {
+        return(
+            <form onSubmit={this.handleSubmit}>
+                <input placeholder="github login" ref="login"/>
+                <button>Add</button>
+            </form>
+        );
+    }
+});
+
 var Main = React.createClass({
+    getInitialState: function () {
+      return {
+          logins: []
+      };
+    },
+    addCard : function (loginToAdd) {
+        this.setState({logins: this.state.logins.concat(loginToAdd)});
+    },
     render : function () {
+        var cards = this.state.logins.map(function(login){
+            return (<Card login={login} />)
+        });
         return  (
             <div>
-                <Card login="kuldeeparora" />
-                <Card login="spicyj" />
+                <Form addCard={this.addCard} />
+                {cards}
             </div>
         )
     }

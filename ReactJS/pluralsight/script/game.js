83022a78c9daaf3b971f7dd92403e54d12d667e7
Kuldeep Arora <kuldeep.arora@gmail.com>
2016-06-01 20:32:49 +0100

    updated game with button

--- ReactJS/pluralsight/script/game.js
+++ ReactJS/pluralsight/script/game.js
@@ -1,13 +1,17 @@
 var StarsFrame = React.createClass({
     render: function () {
+        var numberOfStars = Math.floor(Math.random()*9)+1;
+        var stars = [];
+
+        for(var i=0; i<numberOfStars; i++){
+            stars.push(
+                <span className="glyphicon glyphicon-star"></span>
+            )
+        }
         return (
             <div id="stars-frame">
                 <div className="well">
-                    <span className="glyphicon glyphicon-star"></span>
-                    <span className="glyphicon glyphicon-star"></span>
-                    <span className="glyphicon glyphicon-star"></span>
-                    <span className="glyphicon glyphicon-star"></span>
-                    <span className="glyphicon glyphicon-star"></span>
+                    {stars}
                 </div>
             </div>
         )
@@ -29,24 +33,59 @@ var AnswerFrame = React.createClass({
         return (
             <div id="answer-frame">
                 <div className="well">
-                    ...
+                    {this.props.selectedNumbers}
                 </div>
             </div>
         )
     }
 });
 
+var NumbersFrame = React.createClass({
+    render: function () {
+        var numbers = [], className,
+            clickNumber = this.props.clickNumber,
+            selectedNumbers = this.props.selectedNumbers;
+
+        for(var i=1; i<=9; i++){
+            className = "number selected-" + (selectedNumbers.indexOf(i)>=0);
+            numbers.push(
+                <div className={className} onClick={clickNumber.bind(null, i)}>
+                    {i}
+                </div>
+            )
+        }
+        return (
+            <div id="numbers-frame">
+                <div className="well">
+                    {numbers}
+                </div>
+            </div>
+        )
+    }
+});
+
+
 var Game = React.createClass({
+    getInitialState: function () {
+        return {selectedNumbers : []}
+    },
+    clickNumber : function (clickedNumber) {
+        this.setState(
+            {selectedNumbers: this.state.selectedNumbers.concat(clickedNumber)}
+        )
+    },
     render: function () {
         return (
           <div id="game">
             <h2>Play Nine</h2>
               <hr />
-              <div class="clearfix">
+              <div className="clearfix">
                   <StarsFrame />
                   <ButtonFrame />
-                  <AnswerFrame />
+                  <AnswerFrame selectedNumbers={this.state.selectedNumbers} />
               </div>
+
+              <NumbersFrame selectedNumbers={this.state.selectedNumbers} clickNumber={this.clickNumber} />
           </div>
         );
     }

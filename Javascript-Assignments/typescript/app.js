d1bd6caff808b744df9c3629c2c952a99b7ecfc3
Kuldeep Arora <kuldeep.arora@gmail.com>
2016-06-14 21:26:43 +0100

    enums & array in typescript

--- Javascript-Assignments/typescript/app.js
+++ Javascript-Assignments/typescript/app.js
@@ -1,8 +1,9 @@
 function GetAllBooks() {
     var books = [
-        { title: 'first', author: 'One', available: false },
-        { title: 'second', author: 'two', available: true },
-        { title: 'third', author: 'three', available: false }
+        { title: 'first', author: 'One', available: false, category: Category.bio },
+        { title: 'second', author: 'two', available: true, category: Category.poetry },
+        { title: 'third', author: 'three', available: false, category: Category.children },
+        { title: 'fourth', author: 'four', available: false, category: Category.history }
     ];
     return books;
 }
@@ -19,5 +20,32 @@ function LogFirstAvailableBook(books) {
     console.log("Total Books " + numberOfBooks);
     console.log("Available Books is " + availableBook);
 }
-var allBook = GetAllBooks();
-LogFirstAvailableBook(allBook);
+var Category;
+(function (Category) {
+    Category[Category["bio"] = 0] = "bio";
+    Category[Category["poetry"] = 1] = "poetry";
+    Category[Category["children"] = 2] = "children";
+    Category[Category["history"] = 3] = "history";
+    Category[Category["fiction"] = 4] = "fiction";
+})(Category || (Category = {}));
+function GetBookTitleByCategory(categoryFilter) {
+    console.log("Getting books in category " + categoryFilter);
+    var allBook = GetAllBooks();
+    var filteredTitles = [];
+    // LogFirstAvailableBook(allBook);
+    for (var _i = 0, allBook_1 = allBook; _i < allBook_1.length; _i++) {
+        var currentBook = allBook_1[_i];
+        if (currentBook.category === categoryFilter) {
+            filteredTitles.push(currentBook.title);
+        }
+    }
+    return filteredTitles;
+}
+function LogBookTitles(titles) {
+    for (var _i = 0, titles_1 = titles; _i < titles_1.length; _i++) {
+        var title = titles_1[_i];
+        console.log(title);
+    }
+}
+var poetryBooks = GetBookTitleByCategory(Category.poetry);
+LogBookTitles(poetryBooks);
--- Javascript-Assignments/typescript/app.ts
+++ Javascript-Assignments/typescript/app.ts
@@ -1,11 +1,10 @@
 function GetAllBooks(){
     let books = [
-        {title : 'first', author : 'One', available : false},
-        {title : 'second', author : 'two', available : true},
-        {title : 'third', author : 'three', available : false},
-        {title : 'fourth', author : 'four', available : false}
+        {title : 'first', author : 'One', available : false, category: Category.bio},
+        {title : 'second', author : 'two', available : true, category: Category.poetry},
+        {title : 'third', author : 'three', available : false, category: Category.children},
+        {title : 'fourth', author : 'four', available : false, category: Category.history}
     ];
-
     return books;
 }
 
@@ -24,5 +23,29 @@ function LogFirstAvailableBook(books) : void {
     console.log("Available Books is " + availableBook);
 }
 
-const allBook = GetAllBooks();
-LogFirstAvailableBook(allBook);
+enum Category {bio, poetry, children, history, fiction}
+
+function GetBookTitleByCategory(categoryFilter: Category): Array<string> {
+    console.log("Getting books in category " + Category[categoryFilter]);
+
+    const allBook = GetAllBooks();
+    const filteredTitles : string[] = [];
+    // LogFirstAvailableBook(allBook);
+
+    for (let currentBook of allBook){
+        if(currentBook.category === categoryFilter){
+            filteredTitles.push(currentBook.title)
+        }
+    }
+    return filteredTitles;
+}
+
+function LogBookTitles(titles: string[]): void{
+    for (let title of titles){
+        console.log(title)
+    }
+}
+
+const poetryBooks = GetBookTitleByCategory(Category.poetry);
+
+LogBookTitles(poetryBooks);
